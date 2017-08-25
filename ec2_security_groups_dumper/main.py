280685a51734ad04124388be1a1f098089f1d0cc
Laurent Raufaste <analogue@glop.org>
2017-08-25 15:16:18 -0700

    PEP8 fix

--- ec2_security_groups_dumper/main.py
+++ ec2_security_groups_dumper/main.py
@@ -59,7 +59,8 @@ class FirewallRule(object):
         assert isinstance(name, unicode)
         assert isinstance(description, unicode)
         assert rules_direction in ('INGRESS', 'EGRESS', None)
-        assert rules_ip_protocol in (u'tcp', u'udp', u'icmp', u'icmpv6', "-1", None)
+        assert rules_ip_protocol in (
+            u'tcp', u'udp', u'icmp', u'icmpv6', "-1", None)
         assert isinstance(rules_from_port, (unicode, NoneType))
         assert isinstance(rules_to_port, (unicode, NoneType))
         assert isinstance(rules_grants_group_id, (unicode, NoneType))
