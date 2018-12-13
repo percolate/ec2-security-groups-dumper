766a752086f5098005adabbcae5038681dc5cff0
Robin Kearney <robin@kearney.co.uk>
2018-12-13 22:51:16 +0000

    Triggering CI

--- ec2_security_groups_dumper/main.py
+++ ec2_security_groups_dumper/main.py
@@ -25,9 +25,9 @@ import json
 import csv
 from docopt import docopt
 try:
-    import StringIO
+    import StringIO # Python 2
 except ImportError:
-    import io as StringIO
+    import io as StringIO # Python 3
 
 
 class FirewallRule(object):
